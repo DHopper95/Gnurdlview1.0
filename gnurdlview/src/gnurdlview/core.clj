d542e941234169b0249d3bbe5f79683fd3ae61fa
Hoppy <hoppy@gnurdle.com>
2019-10-16 22:24:13 -0500

    read the test file

--- gnurdlview/src/gnurdlview/core.clj
+++ gnurdlview/src/gnurdlview/core.clj
@@ -1,5 +1,16 @@
 (ns gnurdlview.core
+  (:require [clojure.java.io :as cjio])
   (:gen-class))
+
+(defn read-test-file
+  []
+  (with-open [fd (cjio/reader "resources/test1.data")]
+    (doall (line-seq fd))))
+
+(comment 
+  (count (read-test-file))
+  )
+
 
 (defn -main
   "I don't do a whole lot ... yet."
